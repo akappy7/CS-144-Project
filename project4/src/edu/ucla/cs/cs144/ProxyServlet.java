0adee3b5abd4d0604ad3fe8a98c85bfce75f68d4
akappy7 <akapbasov@gmail.com>
2017-03-02 21:17:27 -0800

    Proxy Servlet added by not tested

--- project4/src/edu/ucla/cs/cs144/ProxyServlet.java
+++ project4/src/edu/ucla/cs/cs144/ProxyServlet.java
@@ -9,11 +9,35 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 public class ProxyServlet extends HttpServlet implements Servlet {
-       
+
     public ProxyServlet() {}
 
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException
     {
-        // your codes here
+        String query = request.getParameter("query");
+        String url = "http://google.com/complete/search?output=toolbar&q=" + query;
+
+        URL url = new URL(url);
+        HttpURLConnection c1 = (HttpURLConnection) url.openConnection();
+
+        // optional default is GET
+		    c1.setRequestMethod("GET");
+		    //add request header
+		    c1.setRequestProperty("User-Agent", USER_AGENT);
+        if (c1.getResponseCode() == HttpURLConnection.HTTP_OK) {
+                BufferedReader br1 = new BufferedReader(new InputStreamReader(c1.getInputStream()));
+                String line;
+                String total;
+
+                while ((line = br1.readLine()) != null) {
+                    total += line;
+                }
+
+                response.setContentType("text/xml");
+                response.getWriter().write(total);
+                //close BufferedReader and c1
+                br1.close();
+                c1.disconnect();
+        }
     }
 }
